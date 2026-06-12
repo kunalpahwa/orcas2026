bdff43f56be828f36beb5b96959e02c7656ae57f
Kunal <kunal.pahwa@gmail.com>
2026-06-12 01:25:32 -0700

    Orcas 2026 trip site

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Orcas2026 from './Orcas2026.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><Orcas2026 /></React.StrictMode>
+)
